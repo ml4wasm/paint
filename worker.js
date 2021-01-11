4ccf66b08250f6d13113d867002deffe8f2dd912
v <>
2021-01-10 20:13:30 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,17 +2,6 @@ importScripts('vecs.js');
 
 const r = Object.values(random)
 
-var rand = r.slice();
-var l = rand.length;
-
-var send = []
-for (var i=0;i<l;i++){
-  var n = Math.round(rand[i]);
-  send.push(n)
-}
-send.length = 640000;
-send.fill(255,639999, 640000);
-
 if (send[0]!==0&&send[0]!==""&&send[0]!==null&&send[0]!==undefined){
   console.log(send);
   self.postMessage(send);
