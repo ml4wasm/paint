6b8d333f62e3895495a586891588d0c06e7525e9
v <>
2021-01-10 20:17:09 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,6 +3,6 @@ importScripts('vecs.js');
 const r = Object.values(random)
 
 if (r[0]!==0&&r[0]!==""&&r[0]!==null&&r[0]!==undefined){
-  console.log(send);
-  self.postMessage(send);
+  console.log(r);
+  self.postMessage(r);
 }
