6f7a6804b857bc4a543cca49b197c35d3080a6c3
v <>
2021-01-10 19:53:18 -0500

    format array

--- worker.js
+++ worker.js
@@ -2,6 +2,12 @@ importScripts('vecs.js');
 
 const rand = Object.values(random)
 
+var l = rand.length;
+
+for (var i=0;i<l;i++){
+  rand[i]=Math.round(rand[i])
+}
+rand.fill(255,639999, 640000);
 if (rand[0]!==0){
   console.log(rand[0]);
   self.postMessage(rand);
