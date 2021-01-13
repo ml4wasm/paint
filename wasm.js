528147c57d846f545d63b6e86e876ceb96994062
v <>
2021-01-13 13:27:56 -0500

    Update wasm.js

--- wasm.js
+++ wasm.js
@@ -3,14 +3,16 @@ importScripts("pkg/grok.js");
 const { neural_network }  = wasm_bindgen;
 
 
-wasm_bindgen('pkg/grok_bg.wasm');
-
-
+// wasm_bindgen('pkg/grok_bg.wasm');
+;(async () => {
+  await wasm_bindgen('./pkg/grok_bg.wasm');
+})();
 console.log("loaded wasm")
 
 self.onmessage = function (message){
 
 ;(async () => {
+  await wasm_bindgen('./pkg/grok_bg.wasm');
   const display = await neural_network(message.data[0], message.data[1]); //image, weights
   console.log("sending from wasm 💐")
   self.postMessage(display);
