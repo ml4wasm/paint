661e9622b49058eead95b86c1810197f3045d76c
v <>
2021-01-13 21:52:42 -0500

    refactored to avoid js_sys. hopefully faster

--- pkg/grok.js
+++ pkg/grok.js
@@ -3,170 +3,162 @@ let wasm_bindgen;
     const __exports = {};
     let wasm;
 
-    const heap = new Array(32).fill(undefined);
-
-    heap.push(undefined, null, true, false);
-
-function getObject(idx) { return heap[idx]; }
-
-let heap_next = heap.length;
-
-function dropObject(idx) {
-    if (idx < 36) return;
-    heap[idx] = heap_next;
-    heap_next = idx;
-}
-
-function takeObject(idx) {
-    const ret = getObject(idx);
-    dropObject(idx);
-    return ret;
-}
-
-function addHeapObject(obj) {
-    if (heap_next === heap.length) heap.push(heap.length + 1);
-    const idx = heap_next;
-    heap_next = heap[idx];
-
-    heap[idx] = obj;
-    return idx;
-}
-
-function isLikeNone(x) {
-    return x === undefined || x === null;
-}
-
-let cachegetFloat64Memory0 = null;
-function getFloat64Memory0() {
-    if (cachegetFloat64Memory0 === null || cachegetFloat64Memory0.buffer !== wasm.memory.buffer) {
-        cachegetFloat64Memory0 = new Float64Array(wasm.memory.buffer);
+    let cachegetFloat64Memory0 = null;
+    function getFloat64Memory0() {
+        if (cachegetFloat64Memory0 === null || cachegetFloat64Memory0.buffer !== wasm.memory.buffer) {
+            cachegetFloat64Memory0 = new Float64Array(wasm.memory.buffer);
+        }
+        return cachegetFloat64Memory0;
     }
-    return cachegetFloat64Memory0;
-}
 
-let cachegetInt32Memory0 = null;
-function getInt32Memory0() {
-    if (cachegetInt32Memory0 === null || cachegetInt32Memory0.buffer !== wasm.memory.buffer) {
-        cachegetInt32Memory0 = new Int32Array(wasm.memory.buffer);
+    let WASM_VECTOR_LEN = 0;
+
+    function passArrayF64ToWasm0(arg, malloc) {
+        const ptr = malloc(arg.length * 8);
+        getFloat64Memory0().set(arg, ptr / 8);
+        WASM_VECTOR_LEN = arg.length;
+        return ptr;
     }
-    return cachegetInt32Memory0;
-}
 
-let cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
-
-cachedTextDecoder.decode();
-
-let cachegetUint8Memory0 = null;
-function getUint8Memory0() {
-    if (cachegetUint8Memory0 === null || cachegetUint8Memory0.buffer !== wasm.memory.buffer) {
-        cachegetUint8Memory0 = new Uint8Array(wasm.memory.buffer);
+    let cachegetInt32Memory0 = null;
+    function getInt32Memory0() {
+        if (cachegetInt32Memory0 === null || cachegetInt32Memory0.buffer !== wasm.memory.buffer) {
+            cachegetInt32Memory0 = new Int32Array(wasm.memory.buffer);
+        }
+        return cachegetInt32Memory0;
     }
-    return cachegetUint8Memory0;
-}
 
-function getStringFromWasm0(ptr, len) {
-    return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
-}
-/**
-* @param {Array<any>} input
-* @param {Array<any>} weights
-* @returns {Array<any>}
-*/
-__exports.neural_network = function(input, weights) {
-    var ret = wasm.neural_network(addHeapObject(input), addHeapObject(weights));
-    return takeObject(ret);
-};
+    function getArrayF64FromWasm0(ptr, len) {
+        return getFloat64Memory0().subarray(ptr / 8, ptr / 8 + len);
+    }
+    /**
+    * @param {Float64Array} vec_a
+    * @param {Float64Array} vec_b
+    * @returns {Float64Array}
+    */
+    __exports.elementwise_addition = function(vec_a, vec_b) {
+        try {
+            const retptr = wasm.__wbindgen_export_0.value - 16;
+            wasm.__wbindgen_export_0.value = retptr;
+            var ptr0 = passArrayF64ToWasm0(vec_a, wasm.__wbindgen_malloc);
+            var len0 = WASM_VECTOR_LEN;
+            var ptr1 = passArrayF64ToWasm0(vec_b, wasm.__wbindgen_malloc);
+            var len1 = WASM_VECTOR_LEN;
+            wasm.elementwise_addition(retptr, ptr0, len0, ptr1, len1);
+            var r0 = getInt32Memory0()[retptr / 4 + 0];
+            var r1 = getInt32Memory0()[retptr / 4 + 1];
+            var v2 = getArrayF64FromWasm0(r0, r1).slice();
+            wasm.__wbindgen_free(r0, r1 * 8);
+            return v2;
+        } finally {
+            wasm.__wbindgen_export_0.value += 16;
+        }
+    };
 
-async function load(module, imports) {
-    if (typeof Response === 'function' && module instanceof Response) {
+    /**
+    * @param {Float64Array} vec
+    * @param {number} n
+    * @returns {Float64Array}
+    */
+    __exports.elementwise_scalar_multiplication = function(vec, n) {
+        try {
+            const retptr = wasm.__wbindgen_export_0.value - 16;
+            wasm.__wbindgen_export_0.value = retptr;
+            var ptr0 = passArrayF64ToWasm0(vec, wasm.__wbindgen_malloc);
+            var len0 = WASM_VECTOR_LEN;
+            wasm.elementwise_scalar_multiplication(retptr, ptr0, len0, n);
+            var r0 = getInt32Memory0()[retptr / 4 + 0];
+            var r1 = getInt32Memory0()[retptr / 4 + 1];
+            var v1 = getArrayF64FromWasm0(r0, r1).slice();
+            wasm.__wbindgen_free(r0, r1 * 8);
+            return v1;
+        } finally {
+            wasm.__wbindgen_export_0.value += 16;
+        }
+    };
 
-        if (typeof WebAssembly.instantiateStreaming === 'function') {
-            try {
-                return await WebAssembly.instantiateStreaming(module, imports);
+    /**
+    * @param {Float64Array} image
+    * @param {Float64Array} weights
+    * @returns {Float64Array}
+    */
+    __exports.prediction = function(image, weights) {
+        try {
+            const retptr = wasm.__wbindgen_export_0.value - 16;
+            wasm.__wbindgen_export_0.value = retptr;
+            var ptr0 = passArrayF64ToWasm0(image, wasm.__wbindgen_malloc);
+            var len0 = WASM_VECTOR_LEN;
+            var ptr1 = passArrayF64ToWasm0(weights, wasm.__wbindgen_malloc);
+            var len1 = WASM_VECTOR_LEN;
+            wasm.prediction(retptr, ptr0, len0, ptr1, len1);
+            var r0 = getInt32Memory0()[retptr / 4 + 0];
+            var r1 = getInt32Memory0()[retptr / 4 + 1];
+            var v2 = getArrayF64FromWasm0(r0, r1).slice();
+            wasm.__wbindgen_free(r0, r1 * 8);
+            return v2;
+        } finally {
+            wasm.__wbindgen_export_0.value += 16;
+        }
+    };
 
-            } catch (e) {
-                if (module.headers.get('Content-Type') != 'application/wasm') {
-                    console.warn("`WebAssembly.instantiateStreaming` failed because your server does not serve wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n", e);
+    async function load(module, imports) {
+        if (typeof Response === 'function' && module instanceof Response) {
 
-                } else {
-                    throw e;
+            if (typeof WebAssembly.instantiateStreaming === 'function') {
+                try {
+                    return await WebAssembly.instantiateStreaming(module, imports);
+
+                } catch (e) {
+                    if (module.headers.get('Content-Type') != 'application/wasm') {
+                        console.warn("`WebAssembly.instantiateStreaming` failed because your server does not serve wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n", e);
+
+                    } else {
+                        throw e;
+                    }
                 }
             }
-        }
 
-        const bytes = await module.arrayBuffer();
-        return await WebAssembly.instantiate(bytes, imports);
-
-    } else {
-
-        const instance = await WebAssembly.instantiate(module, imports);
-
-        if (instance instanceof WebAssembly.Instance) {
-            return { instance, module };
+            const bytes = await module.arrayBuffer();
+            return await WebAssembly.instantiate(bytes, imports);
 
         } else {
-            return instance;
+
+            const instance = await WebAssembly.instantiate(module, imports);
+
+            if (instance instanceof WebAssembly.Instance) {
+                return { instance, module };
+
+            } else {
+                return instance;
+            }
         }
     }
-}
 
-async function init(input) {
-    if (typeof input === 'undefined') {
-        let src;
-        if (typeof document === 'undefined') {
-            src = location.href;
-        } else {
-            src = document.currentScript.src;
+    async function init(input) {
+        if (typeof input === 'undefined') {
+            let src;
+            if (typeof document === 'undefined') {
+                src = location.href;
+            } else {
+                src = document.currentScript.src;
+            }
+            input = src.replace(/\.js$/, '_bg.wasm');
         }
-        input = src.replace(/\.js$/, '_bg.wasm');
-    }
-    const imports = {};
-    imports.wbg = {};
-    imports.wbg.__wbindgen_object_drop_ref = function(arg0) {
-        takeObject(arg0);
-    };
-    imports.wbg.__wbindgen_number_new = function(arg0) {
-        var ret = arg0;
-        return addHeapObject(ret);
-    };
-    imports.wbg.__wbg_new_9dff83a08f5994f3 = function() {
-        var ret = new Array();
-        return addHeapObject(ret);
-    };
-    imports.wbg.__wbg_get_5fa3f454aa041e6e = function(arg0, arg1) {
-        var ret = getObject(arg0)[arg1 >>> 0];
-        return addHeapObject(ret);
-    };
-    imports.wbg.__wbg_length_d2491466819b6271 = function(arg0) {
-        var ret = getObject(arg0).length;
-        return ret;
-    };
-    imports.wbg.__wbg_push_3ddd8187ff2ff82d = function(arg0, arg1) {
-        var ret = getObject(arg0).push(getObject(arg1));
-        return ret;
-    };
-    imports.wbg.__wbindgen_number_get = function(arg0, arg1) {
-        const obj = getObject(arg1);
-        var ret = typeof(obj) === 'number' ? obj : undefined;
-        getFloat64Memory0()[arg0 / 8 + 1] = isLikeNone(ret) ? 0 : ret;
-        getInt32Memory0()[arg0 / 4 + 0] = !isLikeNone(ret);
-    };
-    imports.wbg.__wbindgen_throw = function(arg0, arg1) {
-        throw new Error(getStringFromWasm0(arg0, arg1));
-    };
+        const imports = {};
 
-    if (typeof input === 'string' || (typeof Request === 'function' && input instanceof Request) || (typeof URL === 'function' && input instanceof URL)) {
-        input = fetch(input);
+
+        if (typeof input === 'string' || (typeof Request === 'function' && input instanceof Request) || (typeof URL === 'function' && input instanceof URL)) {
+            input = fetch(input);
+        }
+
+        const { instance, module } = await load(await input, imports);
+
+        wasm = instance.exports;
+        init.__wbindgen_wasm_module = module;
+
+        return wasm;
     }
 
-    const { instance, module } = await load(await input, imports);
-
-    wasm = instance.exports;
-    init.__wbindgen_wasm_module = module;
-
-    return wasm;
-}
-
-wasm_bindgen = Object.assign(init, __exports);
+    wasm_bindgen = Object.assign(init, __exports);
 
 })();
--- wasm.js
+++ wasm.js
@@ -1,6 +1,6 @@
 importScripts("pkg/grok.js");
 
-const { neural_network }  = wasm_bindgen;
+const { prediction }  = wasm_bindgen;
 
 ;(async () => {
   await wasm_bindgen('./pkg/grok_bg.wasm');
@@ -11,7 +11,7 @@ self.onmessage = function (message){
 
 ;(async () => {
   await wasm_bindgen('./pkg/grok_bg.wasm');
-  const display = await neural_network(message.data[0], message.data[1]); //image, weights
+  const display = await prediction(message.data[0], message.data[1]); //image, weights
   console.log("sending from wasm 💐")
   self.postMessage(display);
 
