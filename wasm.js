9b87726034ac4a86ccce141d657369c0bfee48f7
v <>
2021-01-14 08:16:34 -0500

    removed async function ....

--- wasm.js
+++ wasm.js
@@ -9,9 +9,10 @@ console.log("loaded wasm")
 
 self.onmessage = function (message){
 
-;(async () => {
+// ;(async () => {
   const t0 = performance.now();
-  const display = await neural_network(message.data[0], message.data[1]); //image, weights
+  //const display = await neural_network(message.data[0], message.data[1]); //image, weights
+  const display = neural_network(message.data[0], message.data[1]); 
   const t1 = performance.now();
   console.log(`wasm function ${t1 - t0} milliseconds.`);
   console.log("sending from wasm 💐")
@@ -22,6 +23,6 @@ self.onmessage = function (message){
   const t3 = performance.now();
   console.log(`posting message ${t3 - t2} milliseconds.`);
 
-})();
+// })();
 
 }
