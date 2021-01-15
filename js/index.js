0cac270d7695c0052c5699fcddcb4897657963db
v <>
2021-01-15 08:16:19 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-var worker=new Worker("worker.js");var c=document.getElementById("myCanvas");var ctx=c.getContext("2d");var mImgData=ctx.createImageData(300,300);(async()=>{await wasm_bindgen('./pkg/grok_bg.wasm')})();Array.prototype.extend=function(other_array){other_array.forEach(function(v){this.push(v)},this)}
+var worker=new Worker("js/worker.js");var c=document.getElementById("myCanvas");var ctx=c.getContext("2d");var mImgData=ctx.createImageData(300,300);(async()=>{await wasm_bindgen('./pkg/grok_bg.wasm')})();Array.prototype.extend=function(other_array){other_array.forEach(function(v){this.push(v)},this)}
 var encoder=new TextEncoder;const{neural_network}=wasm_bindgen;async function asyncCall(){const display=await neural_network(send,weights);var srcIndex=0,dstIndex=0,curPixelNum=0;for(curPixelNum=0;curPixelNum<300*300;curPixelNum++){mImgData.data[dstIndex]=send[srcIndex];mImgData.data[dstIndex+1]=send[srcIndex+1];mImgData.data[dstIndex+2]=send[srcIndex+2];mImgData.data[dstIndex+3]=255;srcIndex+=3;dstIndex+=4}
 mImgData.data=send;ctx.putImageData(mImgData,0,0)}
 worker.onmessage=function(e){window.send=e.data}
